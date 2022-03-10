6d05189eab30c9498a2a59f3d7e43584c83afae3
Josh Sarnecki <joshsarnecki@gmail.com>
2022-03-10 04:58:32 +0000

    Changed students/teachers node-psql queries to parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -11,14 +11,16 @@ pool.connect();
 
 const cohortMonth = process.argv[2];
 const limit = process.argv[3]
-
-pool.query(`
+const values = [`%${cohortMonth}%`, limit];
+const queryString = `
 SELECT students.id, students.name, cohorts.name AS cohort
 FROM students
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${cohortMonth}%'
-LIMIT ${limit};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach((user) => {
     console.log(`${user.name} has an id of ${user.id}, and was in cohort ${user.cohort}`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,19 +9,21 @@ const pool = new Pool({
 
 pool.connect(console.log('Connected'));
 
-const cohortMonth = process.argv[2];
 // const limit = process.argv[3]
-
-pool.query(`
+const cohortMonth = process.argv[2];
+const values = [cohortMonth];
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, COUNT(assistance_requests) AS total_assistances
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${cohortMonth}'
+WHERE cohorts.name = $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teacher;
-`)
+`;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach((user) => {
     console.log(`${user.cohort}: ${user.teacher}`);
